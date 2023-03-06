c46c89241e9c0b2e8ca9e8daaf744292ac9dab95
rahimabd13 <abdulrahim13501@gmail.com>
2023-03-06 16:56:05 +0530

    Added: calculating Daily Wage for an Employee

--- employeewagecomputation.sh
+++ employeewagecomputation.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+#Employee Daily Hour = 8 (Full Time)
+#Employee working for 5 days in week = 5 * 8 = 40 Hrs.
+#Employee Monthly working for 20 days = 5 days in week * 4 weeks = 20 Days
 
-PRESENT=1;
-isPresent=$((RANDOM%2));
 
-if [ $isPresent -eq $PRESENT ]
-then
-	echo "Employee is Present";
-else
-	echo "Employee is Absent";
-fi
+totalSalary=0;
+EMPLOYEE_WAGE_PER_HOUR=20;
+workingHour=0;
+
+for ((day=1;day<=20;day++))
+do
+	isPresent=$((RANDOM%3))
+	case $isPresent in
+		0)
+			echo "Employee is Absent";
+			workingHour=0;
+		;;
+		1)
+			echo "Employee is Present";
+			workingHour=8;
+		;;
+		2)
+			echo "Employee is working as part time";
+			workingHour=4;
+		;;
+	esac
+
+	salary=$(($EMPLOYEE_WAGE_PER_HOUR * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+done
+
+echo "Employee has earned $totalSalary USD in a month";
